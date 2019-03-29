d30c04a6549da427b55b95189970d4bcac460c5e
iwangyantao <iwangyantao@163.com>
2019-03-29 11:37:58 +0800

    添加问价

---
+++ Sources/MSApi.h
@@ -0,0 +1,35 @@
+//
+//  MSApi.h
+//  MicroSliverOperate
+//
+//  Created by 王延涛 on 2017/2/16.
+//  Copyright © 2017年 www.MicroSliverOperate.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class BaseYTKRequest,CCNetResModel;
+///  HTTP Request method.
+//typedef NS_ENUM(NSInteger, BaseRequestMethod) {
+//    BaseRequestMethodGET = 0,
+//    BaseRequestMethodPOST,
+//    BaseRequestMethodHEAD,
+//    BaseRequestMethodPUT,
+//    BaseRequestMethodDELETE,
+//    BaseRequestMethodPATCH,
+//};
+@interface MSApi : NSObject
+@property(nonatomic,assign)BaseYTKRequest* baseRequest;
+
+
+//普通get请求
++ (void)getContentWithParams:(NSDictionary *)dict jsonValidator:(NSDictionary *)jsonValidator  toURL:(NSString *)url  baseURL:(NSString *)baseURL outTime:(NSTimeInterval)outTime onComplete:(void (^)(BOOL success, CCNetResModel *responseData,NSError *error))block;
+
+//普通get请求
++ (void)getContentWithParams:(NSDictionary *)dict jsonValidator:(NSDictionary *)jsonValidator  toURL:(NSString *)url  baseURL:(NSString *)baseURL onComplete:(void (^)(BOOL success, CCNetResModel *responseData,NSError *error))block;
+//普通post请求
++ (void)sendPostContent:(NSDictionary *)dict jsonValidator:(NSDictionary *)jsonValidator  toURL:(NSString*)url baseURL:(NSString*)baseURL onComplete:(void (^)(BOOL success, CCNetResModel *responseData, NSError *error))block;
+
+//普通post请求
++ (void)sendPostContent:(NSDictionary *)dict jsonValidator:(NSDictionary *)jsonValidator  toURL:(NSString*)url baseURL:(NSString*)baseURL outTime:(NSTimeInterval)outTime onComplete:(void (^)(BOOL success, CCNetResModel *responseData, NSError *error))block;
+
+@end
